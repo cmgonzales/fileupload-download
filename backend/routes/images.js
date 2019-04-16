c323e606db35769c8a13b4c7a67a646380a6ed75
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-04-15 20:49:37 -0700

    reading files

--- backend/routes/images.js
+++ backend/routes/images.js
@@ -6,10 +6,10 @@ const logs = []
 router.get('/', function(req, res, next) {
   fs.readdir('./public/images', (err, files) => {
     files.forEach(file => {
-        logs.push(files)
-        res.send(logs)
+       logs.push(files)
+        res.send(files)
         
-    });
+   });
   });
 });
 
