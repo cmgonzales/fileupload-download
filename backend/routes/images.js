8f5f77042c1f29b98e1e78fa932856bd657da535
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-04-16 20:30:06 -0700

    added query

--- backend/routes/images.js
+++ backend/routes/images.js
@@ -1,17 +1,25 @@
 const fs = require('fs')
 var express = require('express');
 var router = express.Router();
+var url = require('url')
 
-const logs = []
+
 router.get('/', function(req, res, next) {
-  fs.readdir('./public/images', (err, files) => {
-    files.forEach(file => {
-       logs.push(files)
-        res.send(files)
-        
+
+  var query = url.parse(req.url,true).query;
+  pic = query.image
+  fs.readFile('/public/images/' + pic , (err, res) => {
+  if(err){
+    res.writeHead(400, {'Content-type': 'text/html'});
+    console.log(err)
+    res.end("No Image")
+  }else{
+    res.writeHead(200, {'Content-type': 'image//jpg'});
+    res.end(content)
+  }   
    });
   });
-});
+
 
 module.exports = router;
 
