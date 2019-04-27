cf48bb9b8f6bb2ccba559421990967c98edff4cf
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-04-27 11:34:57 -0700

    added config

--- backend/app.js
+++ backend/app.js
@@ -1,68 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors')
-const fileUpload = require('express-fileupload');
-const bodyParser = require('body-parser');
-var app = express();
-const imagefile = require('./routes/images')
+const express = require('express')
 const cloudinary = require('cloudinary')
+const formData = require('express-form-data')
+const cors = require('cors')
+const { CLIENT_ORIGIN } = require('./config')
 
+const app = express()
 
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET
+})
+  
+app.use(cors({ 
+  origin: CLIENT_ORIGIN 
+})) 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(formData.parse())
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload())
-app.use(cors())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/public/images', express.static(__dirname + '/public/images'));
+app.get('/wake-up', (req, res) => res.send("wake-up page"))
 
-app.use('/images', imagefile )
+app.post('/image-upload', (req, res) => {
 
-app.post('/upload', (req, res, next) => {
-  console.log(req);
-  let imageFile = req.files.file;
-
-  imageFile.mv(`${__dirname}/public/images/${req.body.filename}.jpg`, function(err) {
-    if (err) {
-      return res.status(500).send(err);
-    }
-
-    res.json({file: `public/${req.body.filename}.jpg`});
-  });
-
+  const values = Object.values(req.files)
+  const promises = values.map(image => cloudinary.uploader.upload(image.path))
+  
+  Promise
+    .all(promises)
+    .then(results => res.json(results))
+    .catch((err) => res.status(400).json(err))
 })
 
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(8000 , () =>{
-console.log("listen")
-})
-
-module.exports = app;
+app.listen(process.env.PORT || 8000, () => console.log('listening'))
