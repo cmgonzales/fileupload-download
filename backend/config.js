f521e1b1cd9f0bd1b53c4c6998e3e652b9baf067
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-06-08 21:58:42 -0700

    api fix

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? 'https://react-image-upload.surgejj.sh'
-  : 'http://localhost:3000'
--- src/components/image.js
+++ src/components/image.js
@@ -1,22 +1,58 @@
-import {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 
 
-const image = () => (
-    const [img , setimg] = useState({pic: []})
+const image = () => {
+    const [img , setimg] = useState([])
+    const [version, setversion] = useState([])
 
 
-    useEffect(async () => {
-        const result = await axios(
-          'http://hn.algolia.com/api/v1/search?query=redux',
+
+    async function fetchMyAPI() {
+      let response = await fetch('https://res.cloudinary.com/dcjsivxjw/image/list/profile.json')
+      let data = await response.json()
+
+      setimg(data.resources)
+  
+    }
+
+    useEffect(() => {
+      fetchMyAPI();
+    }, []);
+
+  img.map(ver => {
+      return  setversion(ver.version)
+    })
+console.log(version)
+
+    const public_id = img.map(id => {
+      return id.public_id
+    })
+
+console.log(public_id)
+
+         let imgone = "https://res.cloudinary.com/dcjsivxjw/image/upload/v1557799234/Mickey-Mouse-2.jpg.jpg"
+  let images = "https://res.cloudinary.com/dcjsivxjw/image/upload/v" + version + "/" + public_id + ".jpg"
+
+  console.log(images)
+
+
+         return (
+           <div>
+            <ul>
+             {img.map(item => 
+             <li>
+            
+               alt = "Images"  />
+               </li>
+             )}
+           </ul> 
+      
+          <h1>test</h1>
+    
+          </div>
         );
+         
+      }
 
-    
-    )        
-
-    
-    
-    
-
-)
 
 export default image;
